293b70c79753d4415fdb9c03da36952c2a59dd7c
Renan Gomes <renango2015@gmail.com>
2024-03-05 09:45:53 -0300

    feat: improve search params validation

--- src/helpers/validateSearchParams.ts
+++ src/helpers/validateSearchParams.ts
@@ -1,4 +1,4 @@
-import { type PaymentMethod, paymentMethodSchema } from '@/mock/users';
+import { type PaymentMethod, paymentMethodSchema, paymentOptions } from '@/mock/users';
 import { z } from 'zod';
 
 export const validateDate = (param: string | null, defaultDate: Date) => {
@@ -9,12 +9,14 @@ export const validateDate = (param: string | null, defaultDate: Date) => {
 
 export const validateEmployee = (param: string | null, defaultEmployee: string) => {
   const employee = z.coerce.string().trim().min(1).safeParse(param);
-  if (!param || !employee.success) return defaultEmployee;
+  if (!param || !employee.success || !['Barber1', 'Barber2'].includes(employee.data)) return defaultEmployee;
   return employee.data;
 };
 
 export const validatePaymentMethod = (param: string | null, defaultPaymentMethod: PaymentMethod) => {
-  const employee = paymentMethodSchema.safeParse(param);
-  if (!param || !employee.success) return defaultPaymentMethod;
-  return employee.data;
+  const paymentMethod = paymentMethodSchema.safeParse(param);
+  if (!param || !paymentMethod.success || !paymentOptions.includes(paymentMethod.data)) {
+    return defaultPaymentMethod;
+  }
+  return paymentMethod.data;
 };
