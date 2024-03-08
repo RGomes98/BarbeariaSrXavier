7c2c93f75388b6c1e13e4115573f4348aa355603
Renan Gomes <renango2015@gmail.com>
2024-03-08 14:04:45 -0300

    feat: add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,31 @@
 'use client';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { ThemeButton } from '@/components/ThemeButton';
-import { auth } from '@/firebaseConfig/firebase';
-import { haircuts } from '@/mock/haircuts';
-import { signOut } from 'firebase/auth';
-import { Fragment } from 'react';
-
-import Link from 'next/link';
+import { HaircutGrid } from '@/components/HaircutGrid';
+import { Navbar } from '@/components/Navbar';
+import { Input } from '@/components/ui/input';
+import { Fragment, useState } from 'react';
+import { Search } from 'lucide-react';
 
 export default function Home() {
-  const [user] = useAuthState(auth);
+  const [search, setSearch] = useState('');
 
   return (
-    <div className='flex flex-col items-start'>
-      <ThemeButton />
-      {/* {session?.role === 'EMPLOYEE' && (
-        <Link href='/recebidos' className='mb-6 bg-slate-500 p-4'>
-          Recebidos
-        </Link>
-      )} */}
-      {user ? (
-        <Fragment>
-          <button onClick={() => signOut(auth)}>SAIR</button>
-          <Link href='/agendamentos' className='mb-6 bg-slate-500 p-4'>
-            Agendamentos
-          </Link>
-        </Fragment>
-      ) : (
-        <Link href='/entrar' className='mb-6 bg-slate-500 p-4'>
-          Entrar
-        </Link>
-      )}
-
-      {haircuts.map(({ name, id, price }) => {
-        return (
-          <div key={id} className='flex max-w-[128px] flex-col items-center gap-1 text-center'>
-            <p>Corte: {name}</p>
-            <p>Preço: {price}</p>
-            <Link href={`/corte/${id}`} className='mb-6 bg-green-300 p-4'>
-              Agendar
-            </Link>
+    <Fragment>
+      <Navbar />
+      <div className='flex flex-col items-start px-16 pb-20 pt-12 max-md:px-6 max-md:pb-12 max-md:pt-8'>
+        <div className='mb-6 flex w-full justify-between gap-6 max-md:mb-4 max-sm:flex-col'>
+          <h1 className='text-4xl font-semibold max-md:text-3xl'>Cortes</h1>
+          <div className='relative flex max-w-96 items-center max-sm:max-w-full'>
+            <Input
+              onChange={(event) => setSearch(event.target.value)}
+              placeholder='Pesquisar...'
+              className='w-full'
+            />
+            <Search className='absolute right-3 size-5 opacity-70' />
           </div>
-        );
-      })}
-    </div>
+        </div>
+        <HaircutGrid search={search} />
+      </div>
+    </Fragment>
   );
 }
---
+++ src/components/HaircutGrid.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { usePagination } from '@/hooks/usePagination';
+import { formatToCurrency } from '@/utils/number';
+import { useMounted } from '@/hooks/useMounted';
+import { Card } from '@/components/ui/card';
+import { useState, Fragment } from 'react';
+import { haircuts } from '@/mock/haircuts';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const HaircutGrid = ({ search }: { search: string }) => {
+  const [data, setData] = useState(haircuts);
+
+  const filteredData = data.filter(({ name }) => {
+    return search.trim().length ? name.toLowerCase().includes(search.toLowerCase()) : name === name;
+  });
+
+  const { PaginationSection, CURRENT_PAGE_ITEMS } = usePagination(filteredData);
+  const { isMounted } = useMounted();
+
+  return (
+    <Fragment>
+      {PaginationSection}
+      {isMounted && (
+        <div className='grid w-full grid-flow-row grid-cols-4 gap-10 py-6 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1'>
+          {CURRENT_PAGE_ITEMS.length === 0 && <span>Nenhum corte encontrado.</span>}
+          {CURRENT_PAGE_ITEMS.map(({ id, name, photoUri, price }, index) => {
+            return (
+              <Card key={index} className='flex w-full items-start overflow-hidden'>
+                <Link href={`corte/${id}`} className='flex w-full transform flex-col gap-4 pb-4'>
+                  <div className='relative h-80 w-full rounded-sm'>
+                    <Image fill quality={100} src={photoUri} alt='haircut-image' className='object-cover' />
+                  </div>
+                  <div className='flex items-center justify-between px-4'>
+                    <span className='text-xl font-normal'>{name}</span>
+                    <span className='font-bold'>{formatToCurrency(price)}</span>
+                  </div>
+                </Link>
+              </Card>
+            );
+          })}
+        </div>
+      )}
+      {PaginationSection}
+    </Fragment>
+  );
+};
