a8c24210d98bafb55ad946e5fca2bcd9b2902dcb
Renan Gomes <renango2015@gmail.com>
2024-03-10 00:50:28 -0300

    chore: add firebase remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*',
+        hostname: 'console.firebase.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
       },
     ],
   },
