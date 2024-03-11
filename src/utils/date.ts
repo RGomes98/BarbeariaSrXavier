070fbd93a99356ec723b4ee83b728fe281477ef6
Renan Gomes <renango2015@gmail.com>
2024-03-11 00:01:26 -0300

    feat: implement additional date utility functions for versatile date handling

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,4 @@
-export const formatToDateTime = (date: Date) => {
+export const formatToDateTime = (date?: Date) => {
   const [day, month, year] = Intl.DateTimeFormat('en-US', {
     timeZone: 'America/Sao_Paulo',
     month: '2-digit',
@@ -26,9 +26,41 @@ export const formatDate = (date: Date) => {
 export const formatDateShort = (date: Date) => {
   return new Intl.DateTimeFormat('pt-BR', {
     timeZone: 'America/Sao_Paulo',
-    weekday: 'short',
+    weekday: 'long',
     year: 'numeric',
     day: 'numeric',
-    month: 'short',
+    month: 'long',
   }).format(date);
+};
+
+export const formatDateGetWeekAndDay = (date: Date) => {
+  return new Intl.DateTimeFormat('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
+    weekday: 'long',
+    day: '2-digit',
+    month: 'long',
+  }).format(date);
+};
+
+export const formatDateGetDay = (date: Date) => {
+  return new Intl.DateTimeFormat('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
+    day: '2-digit',
+    month: 'long',
+  }).format(date);
+};
+
+export const formatDateGetHour = (date?: Date) => {
+  return new Intl.DateTimeFormat('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
+    minute: 'numeric',
+    hour: 'numeric',
+  }).format(date);
+};
+
+export const isNotWithinThirtyDaysRange = (date: Date) => {
+  return !(
+    new Date(new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1)) > new Date() &&
+    date < new Date(new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 30))
+  );
 };
