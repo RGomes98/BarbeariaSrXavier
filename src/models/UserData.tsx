2ee7b7f5c2f62fc02cfadcd22b0284b9b38844ac
Renan Gomes <renango2015@gmail.com>
2024-03-12 08:08:27 -0300

    fix: update UserData model

--- src/models/UserData.tsx
+++ src/models/UserData.tsx
@@ -1,15 +1,15 @@
 export interface UserData {
-    accountType: AccountType;
-    createdAt: Date;
-    id: string;
-    name: string;
-    cpf: string;
-    cellphone: string;
-    email: string;   
+  accountType: AccountType;
+  createdAt: Date;
+  id: string;
+  name: string;
+  cpf: string;
+  cellphone: string;
+  email: string;
 }
 
 export enum AccountType {
-    ADMIN = "Administrador" ,
-    USER = "Usuário", 
-    BARBER = "Barbeiro", 
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+  BARBER = 'BARBER',
 }
