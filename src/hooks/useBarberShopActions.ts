0337dee35c2ec2fe083649fe42717b72a88797d6
Renan Gomes <renango2015@gmail.com>
2024-03-12 08:15:28 -0300

    build: fix deployment errors

--- src/hooks/useBarberShopActions.ts
+++ src/hooks/useBarberShopActions.ts
@@ -1,71 +1,42 @@
 import { validateDate, validateEmployee, validatePaymentMethod } from '@/helpers/validateSearchParams';
-import { useRouter, useSearchParams } from 'next/navigation';
-import type { Employee, Haircut, Status } from '@/mock/users';
+import type { Haircut, Status } from '@/mock/users';
 import { type Session } from '@/helpers/getSession';
-import { useStore } from '@/store';
-import { getBarbers } from '@/services/getBarbers';
-import { useEffect, useState } from 'react';
-import { AccountType, UserData } from '@/models/UserData';
-import { createAppointment } from '@/services/CreateAppointment';
-import { PayType } from '@/models/Appointment';
 
-export const useBarberShopActions = () => {
-  const [employee, setEmployee] = useState<Employee>();
-  const updateDatabase = useStore().updateDatabase;
-  const database = useStore().database;
+import { useSearchParams } from 'next/navigation';
+import { UserData } from '@/models/UserData';
+import { Barber } from '@/lib/schemas';
+
+export const useBarberShopActions = (barbers: Barber[]) => {
   const searchParams = useSearchParams();
-  const { push } = useRouter();
 
-  const scheduleEmployee = validateEmployee(searchParams.get('employee'), '');
   const paymentMethod = validatePaymentMethod(searchParams.get('payment'), 'CARD');
   const scheduleDate = validateDate(searchParams.get('date'), new Date());
+  const validEmployees = barbers.map(({ name }) => name);
+  const scheduleEmployee = validateEmployee(searchParams.get('employee'), validEmployees, validEmployees[0]);
 
-  useEffect(() => {
-    getCurrentBarbers();
-  }, []);
-
-  async function getCurrentBarbers() {
-    const barbers = await getBarbers();
-    let emp = barbers.find((employee): employee is Employee => {
-      return employee.name === scheduleEmployee && employee.role === 'EMPLOYEE';
-    });
-    setEmployee(emp);
-  }
+  const employee = barbers.find(({ name }) => name === scheduleEmployee);
 
   const getCurrentSchedule = (hour: number) => {
     return new Date(scheduleDate.getFullYear(), scheduleDate.getMonth(), scheduleDate.getDate(), hour);
   };
 
   const getEmployeeCurrentHourSchedule = (hour: number) => {
-    return employee?.schedules.find(({ date }) => {
-      return date.getTime() === getCurrentSchedule(hour).getTime();
-    });
+    // return employee?.schedules.find(({ date }) => {
+    //   return date.getTime() === getCurrentSchedule(hour).getTime();
+    // });
   };
 
-  const scheduleHaircut = (session: Session, haircut: Haircut, scheduleHour: Date, status: Status, userData : UserData) => {
-    let id = ""
-    if(session != null) id = session.id;
-    employee?.schedules.push({
-      clientId: id,
-      status: status,
-      haircut: haircut,
-      date: scheduleHour,
-      id: crypto.randomUUID(),
-      employeeId: employee.cpf,
-      paymentMethod: paymentMethod,
-    });
-
-    //push('/agendamentos');
-    console.log(employee);
-    //if(employee != undefined){
-      createAppointment(employee, haircut, userData, PayType.PIX, scheduleHour);
-    //}
-    //updateDatabase(database);
+  const scheduleHaircut = (
+    session: Session,
+    haircut: Haircut,
+    scheduleHour: Date,
+    status: Status,
+    userData: UserData,
+  ) => {
+    // createAppointment(employee, haircut, userData, PayType.PIX, scheduleHour);
   };
-
 
   return {
-    employee,
     scheduleDate,
     paymentMethod,
     scheduleHaircut,
