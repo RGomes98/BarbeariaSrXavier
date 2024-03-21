232ff993f9e50e44018066f112822ca1e87bffe3
Renan Gomes <renango2015@gmail.com>
2024-03-21 19:46:50 -0300

    chore: payment API test

--- src/components/TableContent.tsx
+++ src/components/TableContent.tsx
@@ -59,33 +59,31 @@ export const TableContent = ({
 
   const { isMounted } = useMounted();
 
-  const isScheduleNotActive = hour < new Date().getHours() && new Date() >= scheduleDate;
+  const isScheduleNotActive = false;
   const currentHourSchedule = getEmployeeCurrentHourSchedule(hour);
   const isEmployeeBusy =
     currentHourSchedule?.status !== 'CANCELED' && currentHourSchedule?.status !== undefined;
-    
-    const form = useForm<Card>({
-      resolver: zodResolver(CardSchema),
-      defaultValues: {cvv: "000", expiryMonth: 1, expiryYear : 2024, holderName : "", number: "0123456789"},
-    });
 
-    
+  const form = useForm<Card>({
+    resolver: zodResolver(CardSchema),
+    defaultValues: { cvv: '000', expiryMonth: 1, expiryYear: 2024, holderName: '', number: '0123456789' },
+  });
+
   async function onSubmit(formData: Login) {
-   const data = await createPaymentLink(paymentMethod, haircut);
+    const data = await createPaymentLink(paymentMethod, haircut);
   }
 
   async function CreatePayment() {
     const data = await createPaymentLink(paymentMethod, haircut);
-    console.log(data)
-   }
+    console.log(data);
+  }
 
-
-  const handleScheduleHaircut = async() => {
+  const handleScheduleHaircut = async () => {
     if (!session || !employee || isEmployeeBusy || isScheduleNotActive) return;
 
     setIsPaymentActive(true);
-    setIsFormActive(false)
-    const respPayment = makePayment(paymentMethod, haircut)
+    setIsFormActive(false);
+    const respPayment = makePayment(paymentMethod, haircut);
     //setPaymentUrl(respPayment.url)
     //const isPaid = checkIsPaid()
     // handleCreateAppointment({
@@ -102,135 +100,135 @@ export const TableContent = ({
   const handleScheduleBreak = async () => {
     if (!session || !employee || isEmployeeBusy || isScheduleNotActive) return;
     setIsPaymentActive(true);
-    setIsFormActive(false)
+    setIsFormActive(false);
 
-     handleCreateAppointment({
-       paymentMethod,
-       type: 'REGULAR',
-       status: 'BREAK',
-       userId: session.id,
-       haircutId: haircut.id,
-       employeeId: employee.id,
-       appointmentDate: getCurrentSchedule(hour),
-     });
+    handleCreateAppointment({
+      paymentMethod,
+      type: 'REGULAR',
+      status: 'BREAK',
+      userId: session.id,
+      haircutId: haircut.id,
+      employeeId: employee.id,
+      appointmentDate: getCurrentSchedule(hour),
+    });
   };
 
   const handleScheduleHaircutSessionless = async (formData: ScheduleFormType) => {
     if (!employee || isEmployeeBusy || isScheduleNotActive) return;
     const { cpf, name, phone } = formData;
     setIsPaymentActive(true);
-    setIsFormActive(false)
+    setIsFormActive(false);
 
-    const respPayment = await makePayment(paymentMethod, haircut)
-   // setPaymentUrl(respPayment.url)
+    const respPayment = await makePayment(paymentMethod, haircut);
+    // setPaymentUrl(respPayment.url);
 
-
-    // handleCreateAppointment({
-    //   cpf,
-    //   name,
-    //   phone,
-    //   paymentMethod,
-    //   status: 'PENDING',
-    //   type: 'SESSIONLESS',
-    //   haircutId: haircut.id,
-    //   employeeId: employee.id,
-    //   appointmentDate: getCurrentSchedule(hour),
-    // });
+    handleCreateAppointment({
+      cpf,
+      name,
+      phone,
+      paymentMethod,
+      status: 'PENDING',
+      type: 'SESSIONLESS',
+      haircutId: haircut.id,
+      employeeId: employee.id,
+      appointmentDate: getCurrentSchedule(hour),
+    });
   };
 
   return (
     <>
-    <AlertDialog open={isFormActive}>
-      <TableRow
-        className={`relative cursor-pointer hover:border-t hover:brightness-110 max-md:text-xs hover:${getScheduleStatusColor(!isScheduleNotActive ? currentHourSchedule?.status : 'DISABLED')} ${getScheduleStatusColor(!isScheduleNotActive ? currentHourSchedule?.status : 'DISABLED')}`}
-      >
-        <TableCell className='max-md:py-3'>{formatDateGetHour(getCurrentSchedule(hour))}h</TableCell>
-        <TableCell className='max-md:hidden'>{formatDateGetWeekAndDay(getCurrentSchedule(hour))}</TableCell>
-        <TableCell className='md:hidden'>{formatDateGetDay(getCurrentSchedule(hour))}</TableCell>
-        <TableCell className='font-medium max-md:hidden'>
-          {formatScheduleStatus('long', !isScheduleNotActive ? currentHourSchedule?.status : 'DISABLED')}
-        </TableCell>
-        <TableCell className='font-medium md:hidden'>
-          {formatScheduleStatus('short', !isScheduleNotActive ? currentHourSchedule?.status : 'DISABLED')}
-        </TableCell>
-        <TableCell className='text-right'>{scheduleEmployee}</TableCell>
-        {(!isEmployeeBusy || !isScheduleNotActive) && isMounted && (
-          <AlertDialogTrigger
-            onClick={() => setIsFormActive(true)}
-            className='absolute inset-0 h-full w-full'
-          />
-        )}
-      </TableRow>
-      <AlertDialogContent className='max-[550px]:max-w-[90%]'>
-        <AlertDialogHeader>
-          <AlertDialogTitle>
-            {(session?.accountType === 'ADMIN' || session?.accountType === 'EMPLOYEE') && 'Horário de Almoço'}
-            {paymentMethod === 'CASH' &&
-              (session?.accountType === 'USER' || !session) &&
-              'Pagamento em Dinheiro'}
-            {paymentMethod !== 'CASH' &&
-              (session?.accountType === 'USER' || !session) &&
-              'Confirmar Agendamento'}
-          </AlertDialogTitle>
-          <AlertDialogDescription>
-            {(session?.accountType === 'ADMIN' || session?.accountType === 'EMPLOYEE') &&
-              `Tem certeza de que deseja definir o horário ${formatDateShort(getCurrentSchedule(hour))} às ${formatDateGetHour(getCurrentSchedule(hour))} como o seu horário de almoço?`}
-            {paymentMethod === 'CASH' &&
-              (session?.accountType === 'USER' || !session) &&
-              'Tem certeza de que deseja pagar pelo agendamento com dinheiro no momento da visita?'}
-            {paymentMethod !== 'CASH' &&
-              (session?.accountType === 'USER' || !session) &&
-              `Tem certeza de que deseja confirmar o agendamento para o horário ${formatDateShort(getCurrentSchedule(hour))} às ${formatDateGetHour(getCurrentSchedule(hour))}?`}
-            {!session && <ScheduleForm handleScheduleHaircutSessionless={handleScheduleHaircutSessionless} />}
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel onClick={() => setIsFormActive(false)}>Cancelar</AlertDialogCancel>
-          {(session?.accountType === 'ADMIN' || session?.accountType === 'EMPLOYEE') && (
-            <AlertDialogAction onClick={handleScheduleBreak}>
-              Definir como Horário de Almoço
-            </AlertDialogAction>
+      <AlertDialog open={isFormActive}>
+        <TableRow
+          className={`relative cursor-pointer hover:border-t hover:brightness-110 max-md:text-xs hover:${getScheduleStatusColor(!isScheduleNotActive ? currentHourSchedule?.status : 'DISABLED')} ${getScheduleStatusColor(!isScheduleNotActive ? currentHourSchedule?.status : 'DISABLED')}`}
+        >
+          <TableCell className='max-md:py-3'>{formatDateGetHour(getCurrentSchedule(hour))}h</TableCell>
+          <TableCell className='max-md:hidden'>{formatDateGetWeekAndDay(getCurrentSchedule(hour))}</TableCell>
+          <TableCell className='md:hidden'>{formatDateGetDay(getCurrentSchedule(hour))}</TableCell>
+          <TableCell className='font-medium max-md:hidden'>
+            {formatScheduleStatus('long', !isScheduleNotActive ? currentHourSchedule?.status : 'DISABLED')}
+          </TableCell>
+          <TableCell className='font-medium md:hidden'>
+            {formatScheduleStatus('short', !isScheduleNotActive ? currentHourSchedule?.status : 'DISABLED')}
+          </TableCell>
+          <TableCell className='text-right'>{scheduleEmployee}</TableCell>
+          {(!isEmployeeBusy || !isScheduleNotActive) && isMounted && (
+            <AlertDialogTrigger
+              onClick={() => setIsFormActive(true)}
+              className='absolute inset-0 h-full w-full'
+            />
           )}
-          {paymentMethod === 'CASH' && (
-            <Fragment>
-              {session?.accountType === 'USER' && (
-                <AlertDialogAction onClick={handleScheduleHaircut}>Pagar com Dinheiro</AlertDialogAction>
-              )}
+        </TableRow>
+        <AlertDialogContent className='max-[550px]:max-w-[90%]'>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              {(session?.accountType === 'ADMIN' || session?.accountType === 'EMPLOYEE') &&
+                'Horário de Almoço'}
+              {paymentMethod === 'CASH' &&
+                (session?.accountType === 'USER' || !session) &&
+                'Pagamento em Dinheiro'}
+              {paymentMethod !== 'CASH' &&
+                (session?.accountType === 'USER' || !session) &&
+                'Confirmar Agendamento'}
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              {(session?.accountType === 'ADMIN' || session?.accountType === 'EMPLOYEE') &&
+                `Tem certeza de que deseja definir o horário ${formatDateShort(getCurrentSchedule(hour))} às ${formatDateGetHour(getCurrentSchedule(hour))} como o seu horário de almoço?`}
+              {paymentMethod === 'CASH' &&
+                (session?.accountType === 'USER' || !session) &&
+                'Tem certeza de que deseja pagar pelo agendamento com dinheiro no momento da visita?'}
+              {paymentMethod !== 'CASH' &&
+                (session?.accountType === 'USER' || !session) &&
+                `Tem certeza de que deseja confirmar o agendamento para o horário ${formatDateShort(getCurrentSchedule(hour))} às ${formatDateGetHour(getCurrentSchedule(hour))}?`}
               {!session && (
-                <AlertDialogAction type='submit' form='sessionless'>
-                  Pagar com Dinheiro
-                </AlertDialogAction>
+                <ScheduleForm handleScheduleHaircutSessionless={handleScheduleHaircutSessionless} />
               )}
-            </Fragment>
-          )}
-          {paymentMethod !== 'CASH' && (
-            <Fragment>
-              {session?.accountType === 'USER' && (
-                <AlertDialogAction onClick={handleScheduleHaircut}>Confirmar Agendamento</AlertDialogAction>
-              )}
-              {!session && (
-                <AlertDialogAction type='submit' form='sessionless'>
-                  Confirmar Agendamento
-                </AlertDialogAction>
-              )}
-            </Fragment>
-          )}
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  
-    {
-    //alert payment 
-    }            
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setIsFormActive(false)}>Cancelar</AlertDialogCancel>
+            {(session?.accountType === 'ADMIN' || session?.accountType === 'EMPLOYEE') && (
+              <AlertDialogAction onClick={handleScheduleBreak}>
+                Definir como Horário de Almoço
+              </AlertDialogAction>
+            )}
+            {paymentMethod === 'CASH' && (
+              <Fragment>
+                {session?.accountType === 'USER' && (
+                  <AlertDialogAction onClick={handleScheduleHaircut}>Pagar com Dinheiro</AlertDialogAction>
+                )}
+                {!session && (
+                  <AlertDialogAction type='submit' form='sessionless'>
+                    Pagar com Dinheiro
+                  </AlertDialogAction>
+                )}
+              </Fragment>
+            )}
+            {paymentMethod !== 'CASH' && (
+              <Fragment>
+                {session?.accountType === 'USER' && (
+                  <AlertDialogAction onClick={handleScheduleHaircut}>Confirmar Agendamento</AlertDialogAction>
+                )}
+                {!session && (
+                  <AlertDialogAction type='submit' form='sessionless'>
+                    Confirmar Agendamento
+                  </AlertDialogAction>
+                )}
+              </Fragment>
+            )}
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
 
-    <AlertDialog  open={isPaymentActive}>
-      <AlertDialogContent className='max-[550px]:max-w-[90%]'>
-        <AlertDialogHeader>
-          <AlertDialogTitle>
-            Fazer Pagamento
-          </AlertDialogTitle>
-       </AlertDialogHeader>
-       {/* {paymentMethod !== 'CARD' && (
+      {
+        //alert payment
+      }
+
+      <AlertDialog open={isPaymentActive}>
+        <AlertDialogContent className='max-[550px]:max-w-[90%]'>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Fazer Pagamento</AlertDialogTitle>
+          </AlertDialogHeader>
+          {/* {paymentMethod !== 'CARD' && (
               <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className='flex flex-col gap-2'>
                   <FormField
@@ -300,19 +298,16 @@ export const TableContent = ({
               </Form>
             )} */}
           <AlertDialogDescription>
-          Acesse esse link para efetuar o pagamento <br></br>
-          {paymentUrl}
-
+            Acesse esse link para efetuar o pagamento <br></br>
+            {paymentUrl}
           </AlertDialogDescription>
 
-        <AlertDialogFooter>
-          <AlertDialogCancel onClick={() => setIsPaymentActive(false)}>Cancelar</AlertDialogCancel>
-          <AlertDialogAction onClick={handleScheduleBreak}>
-             Confirmar Pagamento
-            </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setIsPaymentActive(false)}>Cancelar</AlertDialogCancel>
+            <AlertDialogAction onClick={handleScheduleBreak}>Confirmar Pagamento</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 };
--- src/services/CreatePaymentLink.ts
+++ src/services/CreatePaymentLink.ts
@@ -3,48 +3,52 @@ import { Haircut, PaymentMethod } from '@/lib/schemas';
 import axios from 'axios';
 
 export const createPaymentLink = async (paymentType: PaymentMethod, hairCut: Haircut) => {
-//   const api = axios.create({
-//  //   baseURL: 'https://api.asaas.com/api/v3',
-//     baseURL: 'https://sandbox.asaas.com/api/v3',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       access_token:
-//         '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0MDE4MDQ6OiRhYWNoX2JhNzMyYjlhLWQxMGUtNDY4OS05MjMxLTA1NDY3YzBlZmQ1YQ==',
-//       'Access-Control-Allow-Origin': 'http://localhost:3000', // Allow CORS for localhost
-//       'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-//       'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-//       'Access-Control-Allow-Credentials': 'true',
-//     },
-//   });
+  //   const api = axios.create({
+  //  //   baseURL: 'https://api.asaas.com/api/v3',
+  //     baseURL: 'https://sandbox.asaas.com/api/v3',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       access_token:
+  //         '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0MDE4MDQ6OiRhYWNoX2JhNzMyYjlhLWQxMGUtNDY4OS05MjMxLTA1NDY3YzBlZmQ1YQ==',
+  //       'Access-Control-Allow-Origin': 'http://localhost:3000', // Allow CORS for localhost
+  //       'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+  //       'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+  //       'Access-Control-Allow-Credentials': 'true',
+  //     },
+  //   });
 
-   const paymentData = {
-     billingType: paymentType === 'PIX' ? 'PIX' : 'CREDIT_CARD',
-     chargeType: 'DETACHED',
-     name: hairCut.name,
-     description: hairCut.description,
-     dueDateLimitDays: 10,
-     value: hairCut.price,
-     notificationEnabled: false,
-   };
-
-   const options = {
-    method: 'POST',
-    headers: {accept: 'application/json', 'content-type': 'application/json', access_token:'$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0MDE4MDQ6OiRhYWNoX2JhNzMyYjlhLWQxMGUtNDY4OS05MjMxLTA1NDY3YzBlZmQ1YQ==' },
-    body: JSON.stringify(paymentData)
+  const paymentData = {
+    billingType: paymentType === 'PIX' ? 'PIX' : 'CREDIT_CARD',
+    chargeType: 'DETACHED',
+    name: hairCut.name,
+    description: hairCut.description,
+    dueDateLimitDays: 10,
+    value: hairCut.price,
+    notificationEnabled: false,
   };
-  
-  fetch('https://sandbox.asaas.com/api/v3/paymentLinks', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 
-//   try {
-//     const response = await api.post('/paymentLinks', paymentData);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error creating payment link:', error);
-//     throw error;
-//   }
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      access_token:
+        '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0MDE4MDQ6OiRhYWNoX2JhNzMyYjlhLWQxMGUtNDY4OS05MjMxLTA1NDY3YzBlZmQ1YQ==',
+    },
+    body: JSON.stringify(paymentData),
+  };
+
+  const response = await fetch('https://sandbox.asaas.com/api/v3/paymentLinks', options);
+  const data = await response.json();
+  console.log(data, response, 'hi');
+
+  //   try {
+  //     const response = await api.post('/paymentLinks', paymentData);
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error('Error creating payment link:', error);
+  //     throw error;
+  //   }
 };
 
 export const checkPayment = async () => {
@@ -63,14 +67,11 @@ export const checkPayment = async () => {
   //      'Access-Control-Allow-Credentials': 'true',
   //    },
   //  });
-
   //     // Make the API call to check if payment is made
   //     const response = await api.get('/paymentLinks');
   //     const paymentLinks = response.data;
-
   //     // Check if payment is made for the specific hairCut
   //     const paymentMade = paymentLinks.some((link: any) => link.name === hairCut.name && link.status === 'RECEIVED');
-
   //     if (paymentMade) {
   //       console.log('Payment made!');
   //       return true;
@@ -81,7 +82,6 @@ export const checkPayment = async () => {
   //   } catch (error) {
   //     console.error('Error checking payment:', error);
   //     return false;
-
   //   }
   // }, 10000); // Check every 10 seconds
 };
