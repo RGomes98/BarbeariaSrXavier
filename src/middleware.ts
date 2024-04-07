691df4ab7cac43733b1f89f60d028f6cfadd06dd
Renan Gomes <renango2015@gmail.com>
2024-04-07 12:25:20 -0300

    refactor: enhance session retrieval method on the server

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,20 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 import { User } from './lib/schemas';
 
 export async function middleware(request: NextRequest) {
-  const session = request.cookies.get('session');
+  const session = cookies().get('session');
 
   if (['/entrar', '/registrar'].some((path) => request.nextUrl.pathname.startsWith(path))) {
     if (session) return NextResponse.redirect(new URL('/', request.url));
     return NextResponse.next();
   }
 
-  if (!session) return NextResponse.json({ session: session });
+  if (!session) return NextResponse.redirect(new URL('/entrar', request.url));
 
   const userAuthentication = await fetch(`${request.nextUrl.origin}/api/auth`, {
     headers: { Cookie: `session=${session.value}` },
   });
-
-  if (userAuthentication.status !== 200) return NextResponse.json({ session: userAuthentication });
 
   if (userAuthentication.status !== 200) return NextResponse.redirect(new URL('/entrar', request.url));
 
